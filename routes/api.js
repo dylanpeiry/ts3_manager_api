d7bd2046ffde548183a5040970ebe23eefa82ea6
PeiryD <contact@dylanpeiry.ch>
2019-12-09 19:39:21 +0100

    Added the following routes
    api/auth/login - api/auth/logout - api/clients

--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,51 @@
 let express = require('express');
 let router = express.Router();
 const {TeamSpeak} = require('ts3-nodejs-library');
+
+let authRouter = require('./auth');
 //Specify if the user is currently connected to the serverquery
 let ts3 = null;
-
+let response = {success: false, data: {}};
 
 router.post('/auth/login', ((req, res, next) => {
-    let data = req.body;
-    let login = data.login;
-    let password = data.password;
     TeamSpeak.connect({
         host: "ts.s0kke.ch",
         queryport: 10011,
         serverport: 9987,
-        username: login,
-        password: password,
+        username: req.body.login,
+        password: req.body.password,
         nickname: "NodeJS"
     }).then(teamspeak => {
         ts3 = teamspeak;
-        let response = {success: true, data: {message: "Connection successful."}};
+        response.success = true;
+        response.data.message = "Connection successful.";
         res.send(response);
     }).catch(e => {
         ts3 = null;
-        let response = {success: false, data: e};
+        response.success = false;
+        response.data = e;
         res.send(response);
     })
+
 }));
 
 router.post('/auth/logout', ((req, res, next) => {
     ts3 = null;
-    let response = {success: true, data: {message: 'Disconnected.'}};
+    response.success = true;
+    response.data.message = "Disconnected.";
     res.send(response);
 }));
 
 router.get('/clients', async (req, res, next) => {
-    if (ts3 != null)
-        res.send(await ts3.clientDBList());
-    else
-        res.send('No Connection');
+    if (ts3 != null) {
+        response.success = true;
+        response.data = await ts3.clientDBList();
+        res.send(response);
+    } else {
+        response.success = false;
+        response.data.message = "No connection.";
+        res.send(response);
+    }
 });
 
 router.post('/clients', ((req, res, next) => {
